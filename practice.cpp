ec3c9e85e89a402c6da60a17d25ee61d9e2b8583
Pritam <pritammagdum24@gmail.com>
2024-08-15 20:09:20 +0530

    Rotate Array by Right of K Value

---
+++ practice.cpp
@@ -0,0 +1,33 @@
+// Right rotate an array by k place
+
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+void leftRotate(int arr[], int n, int d)
+{
+    reverse(arr, arr + d);
+    reverse(arr + d, arr + n);
+    reverse(arr, arr + n);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int d;
+    cin >> d;
+    leftRotate(arr, n, d);
+
+    // to show output
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
