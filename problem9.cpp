ad58939b8152e0601fb87cd19d27e5703486df80
Pritam <pritammagdum24@gmail.com>
2024-08-15 22:06:39 +0530

    Union of Two Sorted Array - Method  2 Optimal Approach

--- problem9.cpp
+++ problem9.cpp
@@ -1,5 +1,6 @@
 // Union of Two sorted array
 
+// Method 1
 vector<int> findUnion(int arr1[], int arr2[], int n, int m)
 {
     set<int> st;
@@ -18,4 +19,51 @@ vector<int> findUnion(int arr1[], int arr2[], int n, int m)
         temp.push_back(it);
     }
     return temp;
+}
+
+// Method 2
+vector<int> SortedArray(vector<int> a, vector<int> b)
+{
+    int n1 = a.size();
+    int n2 = b.size();
+    int i = 0;
+    int j = 0;
+    vector<int> uinonArr;
+    while (i < n1 && j < n2)
+    {
+        if (a[i] <= b[j])
+        {
+            if (unionArr.size() == 0 || unionArr.back() != a[i])
+            {
+                unionArr.push_back(a[i]);
+            }
+            i++;
+        }
+        else
+        {
+            if (unionArr.size() == 0 || unionArr.back() != b[j])
+            {
+                unionArr.push_back(b[j]);
+            }
+            j++;
+        }
+    }
+
+    while (j < n2)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != b[j])
+        {
+            unionArr.push_back(b[j]);
+        }
+        j++;
+    }
+
+    while (i < n1)
+    {
+        if (unionArr.size() == 0 || unionArr.back() != a[i])
+        {
+            unionArr.push_back(a[i]);
+        }
+        i++;
+    }
 }
